76eaefac45a8dd9bcf22253e4e6d6acbc25d1f3b
IbrohimKhasanov <ibrohimdev01@gmail.com>
2022-08-30 13:29:58 +0300

    1.2 Beta

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var burgerBox = document.querySelector('.burger-box')
+function burgerOpen(){
+    burgerBox.style.display = 'block';
+}
+function burgerClose(){
+    burgerBox.style.display = 'none';
+}
