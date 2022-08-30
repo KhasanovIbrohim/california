7a42a76a5095584e5668410fe1e9702ddef75439
IbrohimKhasanov <ibrohimdev01@gmail.com>
2022-08-30 17:38:07 +0300

    1.3.0 Global

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 var burgerBox = document.querySelector('.burger-box')
+var closeBtn = document.querySelector('.burger-close')
+var openBtn = document.querySelector('.burger-btn')
 function burgerOpen(){
     burgerBox.style.display = 'block';
+    closeBtn.style.display = 'block';
+    openBtn.style.display = 'none';
 }
 function burgerClose(){
     burgerBox.style.display = 'none';
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'block';
 }
