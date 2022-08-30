d9be1d9012adee277b5a7422f241700ad2eeedd6
IbrohimKhasanov <ibrohimdev01@gmail.com>
2022-08-30 20:45:17 +0300

    1.4 Global

--- js/script.js
+++ js/script.js
@@ -11,4 +11,33 @@ function burgerClose(){
     burgerBox.style.display = 'none';
     closeBtn.style.display = 'none';
     openBtn.style.display = 'block';
-}
+}   
+var introTitle = document.querySelector('.intro-title')
+var introText = document.querySelector('.intro-text')
+var introPhone = document.querySelector('.intro-phone')
+var introWatch = document.querySelector('.intro-watch')
+var introLaptop = document.querySelector('.intro-laptop')
+
+setInterval(function () {
+    introTitle.textContent = "The best quality and price."
+    introText.textContent = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+    introPhone.style.display = "none";
+    introWatch.style.display = "none";
+    introLaptop.style.display = "block";
+}, 4000);
+
+setInterval(function () {
+    introTitle.textContent = "Ideas have a place here..."
+    introText.textContent = "We Make It Easy To Find The Great Design Talent, Easier... Road Design Handbook For The International Road..."
+    introPhone.style.display = "none";
+    introWatch.style.display = "block";
+    introLaptop.style.display = "none";
+}, 8000);
+
+setInterval(function () {
+    introTitle.textContent = "The new phones are here take a look."
+    introText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est dui, aliquam, tempor. Faucibus morbi turpis."
+    introPhone.style.display = "block";
+    introWatch.style.display = "none";
+    introLaptop.style.display = "none";
+}, 12000);
